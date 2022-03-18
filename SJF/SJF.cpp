4aae56388d010a671541f5e2df5a780b19687eaa
Mehrdad Jafari <asedmehrdad@gmail.com>
2022-03-18 11:40:32 +0330

    Added EnqueueReady
    creates a Ready queue and puts the process in the queue

--- SJF/SJF.cpp
+++ SJF/SJF.cpp
@@ -11,19 +11,35 @@ typedef struct
 	char Name[10];
 } Process;
 
+Process Procs[PROC_COUNT];
+
+Process* ReadyQueue[PROC_COUNT];
+int QueueLast = -1;
+
 void InitProcesses(Process*);
+void EnqueueReady(int);
 
 int main()
 {
-	Process Procs[PROC_COUNT];
-	//Process** ProcPtr = &Procs;
 	InitProcesses(Procs);
+	EnqueueReady(2);
+	EnqueueReady(0);
+	EnqueueReady(4);
+	EnqueueReady(3);
+	EnqueueReady(1);
+	
 	printf("Hello World");
 	return 0;
 }
 
 void InitProcesses(Process* procs)
 {
+	procs[0].BurstTime = 1.0f;
+	procs[1].BurstTime = 1.0f;
+	procs[2].BurstTime = 1.0f;
+	procs[3].BurstTime = 1.0f;
+	procs[4].BurstTime = 1.0f;
+
 	int Index = 0;
 	for(; Index < PROC_COUNT; ++Index)
 	{
@@ -33,10 +49,16 @@ void InitProcesses(Process* procs)
 		_itoa_s(Index, Num, 10);
 		strcat_s(procs[Index].Name, Num);
 	}
+}
 
-	procs[0].BurstTime = 1.0f;
-	procs[1].BurstTime = 1.0f;
-	procs[2].BurstTime = 1.0f;
-	procs[3].BurstTime = 1.0f;
-	procs[4].BurstTime = 1.0f;
+void EnqueueReady(int ProcIndex)
+{
+	++QueueLast;
+	if(QueueLast >= PROC_COUNT)
+	{
+		printf("Queue overload");
+		exit(-1);
+	}
+	
+	ReadyQueue[QueueLast] = &Procs[ProcIndex];
 }
